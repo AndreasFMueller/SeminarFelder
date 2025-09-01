cd9f885293adf486d37700c6ad30049d2a7f57a1
P4ntomime <50500745+P4ntomime@users.noreply.github.com>
2025-09-01 14:51:29 +0200

    Graphen für lineares Medium gezeichnet und eingefügt. Referenzen noch nicht angepasst, da noch weitere Grafiken fehlen (Simulation, lineares medium)

--- buch/papers/particles/figures/main.py
+++ buch/papers/particles/figures/main.py
@@ -31,6 +31,11 @@ r"""
 """
 )
 
+def save_wrapped_tikz(filepath, axis_width = None, axis_height = None):
+    tikz_code = tikz_template[0] + matplot2tikz.get_tikz_code(strict = True, wrap = False, axis_width=axis_width, axis_height=axis_height) + tikz_template[1]
+    with open(filepath, "w+", encoding="utf-8") as f:
+        f.write(tikz_code)
+
 def plot_lineares_medium():
     D = 1.0             # Beispielkonstante
     F = np.linspace(-2, 2, 30)
@@ -43,13 +48,21 @@ def plot_lineares_medium():
     plt.ylabel(r'$\Delta l$', loc="center", rotation=0)
     plt.yticks([-2, -1, 0, 1, 2])
     plt.legend(loc="lower right", frameon=True, edgecolor="black", facecolor="white", framealpha=1)
-    # plt.show()
 
-    tikz_code = tikz_template[0] + matplot2tikz.get_tikz_code(wrap = False) + tikz_template[1]
-    with open(output_path / "lineares_medium_plot.tex", "w+", encoding="utf-8") as f:
-        f.write(tikz_code)
+    # plt.show()
+    save_wrapped_tikz(output_path / "lineares_medium_deformation.tex")
         
-    
+    plt.figure()
+    plt.plot(F, len(F) * [D], label=r'$D(F) = 1$ (const.)')
+    plt.xlabel('$F$', loc="center")
+    plt.xticks([-2, -1, 0, 1, 2])
+    plt.ylabel('$D$', labelpad=0, loc="center", rotation=0)  # Reduce margin with labelpad=0
+    plt.ylim((0, 2.1))
+    plt.yticks([0, 1, 2])
+    plt.legend(loc="lower right", frameon=True, edgecolor="black", facecolor="white", framealpha=1)
+
+    # plt.show()
+    save_wrapped_tikz(output_path / "lineares_medium_elast_modul.tex")
     
 
 def plot_nichtlineares_medium():
